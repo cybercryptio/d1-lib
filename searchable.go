88a9ad9abca83527cc886fb917cc2008e94b9e4e
AmalieDue <amalieduejensen@hotmail.com>
2022-05-02 11:51:34 +0200

    added searchable

---
+++ searchable.go
@@ -0,0 +1,159 @@
+// Copyright 2020-2022 CYBERCRYPT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package encryptonize
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+
+	"encryptonize/crypto"
+)
+
+// Index contains a mapping from string label to SealedID.
+type Index struct {
+	mapping map[string]SealedID
+}
+
+// SealedID is an encrypted structure which defines an occurrence of a specific keyword in a specific ID.
+type SealedID struct {
+	ciphertext []byte
+	wrappedKey []byte
+}
+
+const MasterKeyLength = 32
+
+var ErrInvalidMasterKeyLength = fmt.Errorf("invalid key length, accepted key length is %d bytes", MasterKeyLength)
+
+// NewSearchable creates an Index which is used to manage keyword/ID pairs.
+func NewSearchable() Index {
+	mapping := make(map[string]SealedID)
+
+	return Index{mapping: mapping}
+}
+
+// Add is used to add a keyword/ID pair to the Index.
+func (i *Index) Add(key []byte, keyword, id string) error {
+	if len(key) != MasterKeyLength {
+		return ErrInvalidMasterKeyLength
+	}
+
+	k1 := crypto.KMACKDF(crypto.TaggerKeyLength, key, []byte("mac"), []byte(keyword))
+	k2 := crypto.KMACKDF(crypto.EncryptionKeyLength, key, []byte("id"), []byte(keyword))
+
+	tagger, err := crypto.NewKMAC256Tagger(k1)
+	if err != nil {
+		return err
+	}
+	cryptor, err := crypto.NewAESCryptor(k2)
+	if err != nil {
+		return err
+	}
+
+	count, err := i.count(key, keyword)
+	if err != nil {
+		return err
+	}
+	label, err := tagger.Tag(uint64Encode(count))
+	if err != nil {
+		return err
+	}
+
+	wrappedKey, encryptedID, err := cryptor.Encrypt([]byte(id), "")
+	if err != nil {
+		return err
+	}
+	sealedID := SealedID{ciphertext: encryptedID, wrappedKey: wrappedKey}
+
+	i.mapping[base64.StdEncoding.EncodeToString(label)] = sealedID
+
+	return nil
+}
+
+// Given a keyword, Search returns all decrypted ID's that the keyword is contained in.
+func (i *Index) Search(key []byte, keyword string) ([]string, error) {
+	if len(key) != MasterKeyLength {
+		return nil, ErrInvalidMasterKeyLength
+	}
+
+	k1 := crypto.KMACKDF(crypto.TaggerKeyLength, key, []byte("mac"), []byte(keyword))
+	k2 := crypto.KMACKDF(crypto.EncryptionKeyLength, key, []byte("id"), []byte(keyword))
+
+	tagger, err := crypto.NewKMAC256Tagger(k1)
+	if err != nil {
+		return nil, err
+	}
+	cryptor, err := crypto.NewAESCryptor(k2)
+	if err != nil {
+		return nil, err
+	}
+
+	decryptedIDs := []string{}
+
+	for count := 0; ; count++ {
+		label, err := tagger.Tag(uint64Encode(uint64(count)))
+		if err != nil {
+			return nil, err
+		}
+
+		if encryptedID, ok := i.mapping[base64.StdEncoding.EncodeToString(label)]; !ok {
+			break
+		} else {
+			var plaintext []byte
+			err := cryptor.Decrypt(&plaintext, "", encryptedID.wrappedKey, encryptedID.ciphertext)
+			if err != nil {
+				return nil, err
+			}
+			decryptedIDs = append(decryptedIDs, string(plaintext))
+		}
+	}
+
+	return decryptedIDs, nil
+}
+
+func (i *Index) count(key []byte, keyword string) (uint64, error) {
+	if len(key) != MasterKeyLength {
+		return 0, ErrInvalidMasterKeyLength
+	}
+
+	k1 := crypto.KMACKDF(crypto.TaggerKeyLength, key, []byte("mac"), []byte(keyword))
+
+	tagger, err := crypto.NewKMAC256Tagger(k1)
+	if err != nil {
+		return 0, err
+	}
+
+	var count uint64
+
+	for j := 0; ; j++ {
+		label, err := tagger.Tag(uint64Encode(uint64(j)))
+		if err != nil {
+			return 0, err
+		}
+
+		if _, ok := i.mapping[base64.StdEncoding.EncodeToString(label)]; !ok {
+			count = uint64(j)
+			break
+		}
+	}
+
+	return count, nil
+}
+
+func uint64Encode(i uint64) []byte {
+	buf := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutUvarint(buf, i)
+	return buf[:n]
+}
---
+++ searchable_test.go
@@ -0,0 +1,276 @@
+// Copyright 2020-2022 CYBERCRYPT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package encryptonize
+
+import (
+	"reflect"
+	"testing"
+
+	"encryptonize/crypto"
+)
+
+func TestNewSearchable(t *testing.T) {
+	searchable := NewSearchable()
+	if len(searchable.mapping) != 0 {
+		t.Fatal("Index non-empty at initialization.")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keyword := "first keyword"
+	id := "first id"
+
+	err = searchable.Add(masterKey, keyword, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(searchable.mapping) != 1 {
+		t.Fatal("Keyword/id pair not correctly added to mapping")
+	}
+}
+
+func TestAddMultiple(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keywords := [2]string{"first keyword", "second keyword"}
+	ids := [2]string{"first id", "second id"}
+
+	for i := 0; i < len(keywords); i++ {
+		for j := 0; j < len(ids); j++ {
+			err = searchable.Add(masterKey, keywords[i], ids[j])
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	if len(searchable.mapping) != len(keywords)*len(ids) {
+		t.Fatal("Multiple keyword/id pairs not correctly added to mapping")
+	}
+}
+
+func TestSearch(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keyword := "first keyword"
+	ids := []string{"id1", "id2", "id3", "id4", "id5"}
+
+	for i := 0; i < len(ids); i++ {
+		err = searchable.Add(masterKey, keyword, ids[i])
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		IDs, err := searchable.Search(masterKey, keyword)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(IDs[:i], ids[:i]) {
+			t.Fatal("Search returned wrong decrypted IDs.")
+		}
+	}
+}
+
+func TestSearchWrongMasterkey(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey1, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+	masterKey2, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keyword := "first keyword"
+	id := "first id"
+
+	err = searchable.Add(masterKey1, keyword, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	IDs, err := searchable.Search(masterKey2, keyword)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(IDs) != 0 {
+		t.Fatal("Search returned decrypted IDs when given wrong masterkey")
+	}
+}
+
+func TestSearchWrongKeyword(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keyword := "first keyword"
+	id := "first id"
+
+	err = searchable.Add(masterKey, keyword, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keywordShort := "first keywor"
+	keywordLong := "first keywordd"
+
+	IDs, err := searchable.Search(masterKey, keywordShort)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(IDs) != 0 {
+		t.Fatal("Search returned decrypted IDs when given wrong keyword")
+	}
+
+	IDs, err = searchable.Search(masterKey, keywordLong)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(IDs) != 0 {
+		t.Fatal("Search returned decrypted IDs when given wrong keyword")
+	}
+}
+
+func TestCount(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keywords := [3]string{"keyword1", "keyword2", "keyword3"}
+	ids := [5]string{"id1", "id2", "id3", "id4", "id5"}
+
+	for i := 0; i < len(keywords); i++ {
+		count, err := searchable.count(masterKey, keywords[i])
+		if err != nil {
+			t.Fatal(err)
+		}
+		if count != 0 {
+			t.Fatal("Count returned wrong count.")
+		}
+	}
+
+	for i := 0; i < len(keywords); i++ {
+		for j := 0; j < len(ids); j++ {
+			err = searchable.Add(masterKey, keywords[i], ids[j])
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			count, err := searchable.count(masterKey, keywords[i])
+			if err != nil {
+				t.Fatal(err)
+			}
+			if count != uint64(j+1) {
+				t.Fatal("Count returned wrong count.")
+			}
+		}
+	}
+}
+
+func TestCountWrongMasterkey(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey1, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+	masterKey2, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keyword := "first keyword"
+	id := "first id"
+
+	err = searchable.Add(masterKey1, keyword, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	count, err := searchable.count(masterKey2, keyword)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatal("Count returned wrong count when given wrong masterkey.")
+	}
+}
+
+func TestCountWrongKeyword(t *testing.T) {
+	searchable := NewSearchable()
+
+	rand := &crypto.NativeRandom{}
+	masterKey, err := rand.GetBytes(32)
+	if err != nil {
+		t.Fatalf("Random failed: %v", err)
+	}
+
+	keyword := "first keyword"
+	id := "first id"
+
+	err = searchable.Add(masterKey, keyword, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keywordShort := "first keywor"
+	keywordLong := "first keywordd"
+
+	count, err := searchable.count(masterKey, keywordShort)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatal("Count returned wrong count when given wrong keyword.")
+	}
+
+	count, err = searchable.count(masterKey, keywordLong)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatal("Count returned wrong count when given wrong keyword.")
+	}
+}
