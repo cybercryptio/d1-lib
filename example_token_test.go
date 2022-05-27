ce44840074935e67b8e37ef42d45c9a09f9e691d
Philip Vejre <philip@cyber-crypt.com>
2022-05-27 11:07:30 +0200

    Update examples with IO Provider

--- example_token_test.go
+++ example_token_test.go
@@ -23,7 +23,7 @@ import (
 
 func ExampleEncryptonize_CreateToken() {
 	// Instantiate the Encryptonize® library with the given keys.
-	ectnz, err := encryptonize.New(&keyProvider)
+	ectnz, err := encryptonize.New(&keyProvider, &ioProvider)
 	if err != nil {
 		log.Fatalf("Error instantiating Encryptonize: %v", err)
 	}
